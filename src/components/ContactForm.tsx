733780f4d1af76dd7420044ce25aa5c7512b4ff9
rishabh-rapidquest <rishabhc@rapidquest.in>
2024-04-30 11:04:25 +0530

    added cancle button

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -80,7 +80,7 @@ const ContactForm = () => {
                 </label>
                 <div className='mt-8'>
 
-                    <button className="text-white float-left bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-800">Cancel</button>
+                    <button onClick={() => navigate("/contacts")} className="text-white float-left bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-800">Cancel</button>
                     <button type="submit" onClick={handleSubmit} className="text-white float-right bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                 </div>
             </form>
